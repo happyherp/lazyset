29fdf6e94e4ea4208a1155086c5394a979b57207
Carlos Freund <carlosfreund@gmail.com>
2016-11-23 14:26:00 +0100

    put a wrapper around the LazySet-type so it can be used as a class instance

--- LazySet.hs
+++ LazySet.hs
@@ -3,25 +3,66 @@ module LazySet where
 
 import qualified Data.List as List
 import qualified Data.Set as NSet
+import Data.Ord(comparing)
 
-data LazySet a = Node (NSet.Set a) (LazySet a) | End 
-    deriving (Show)
+import qualified Data.Foldable as Foldable
 
+
+data Direction = ASC | DESC
+data LazySet a = LazySet Direction [NSet.Set a]
+    
+instance Foldable.Foldable (LazySet) where 
+
+    foldMap f (LazySet _ sets) =  undefined -- foldMap $  foldMap sets
+    
     
 member :: Ord a => a -> LazySet a ->  Bool
-member e End = False
-member e (Node set nextLevel) = case NSet.lookupLE e set of 
-    Nothing -> False
-    Just x ->  x == e || member e nextLevel
+member e (LazySet dir sets) = findIn sets
+    where findIn [] = False
+          findIn (set:rest) = case lookup e set of 
+            Nothing -> False
+            Just x ->  x == e || findIn rest
+          lookup = case dir of ASC -> NSet.lookupLE; DESC -> NSet.lookupGE
                                  
-                                
+{-         
+lookupLT :: Ord a => a -> LazySet a -> Maybe a
+lookupLT e (LazySet ASC sets) =  let 
+  candidates =  List.takeWhile (\s -> NSet.findMin s < e) sets
+  in foldMap (NSet.lookupLT e) $List.reverse candidates    
+-}  
+
+build :: Ord a => Int -> [a] -> [NSet.Set a]         
+build _ [] = []
+build level xs = let 
+    growth = 2          
+    (elementsForThisLevel, elementsFurtherDown) = List.splitAt (growth^level) xs
+    in (NSet.fromList elementsForThisLevel):(build (level + 1) elementsFurtherDown)
+    
+    
+fromListAsc :: Ord a => [a] -> LazySet a
+fromListAsc xs = LazySet ASC (build 0 (checkDir xs))
+    where checkDir (a:b:s)| a > b = error "Elements must be ascending." 
+          checkDir  xs = xs                  
+
 
 fromList :: Ord a => [a] -> LazySet a
-fromList xs = f 0 (checkAsc xs)
-    where f _ [] = End
-          f level xs = let 
-            growth = 2          
-            (elementsForThisLevel, elementsFurtherDown) = List.splitAt (growth^level) xs
-            in Node (NSet.fromList elementsForThisLevel) (f (level + 1) elementsFurtherDown)
-          checkAsc (a:b:s)| a > b = error "Elements must be ascending." 
-          checkAsc  xs = xs         
+fromList = fromListAsc
+          
+          
+fromListDesc :: Ord a => [a] -> LazySet a
+fromListDesc xs = LazySet DESC (build 0 (checkDir xs))
+    where checkDir (a:b:s)| a < b = error "Elements must be descending." 
+          checkDir  xs = xs          
+          
+      
+--List with all elements in the order of the set.
+toList :: Ord a => LazySet a -> [a]
+toList (LazySet dir sets) = concat $ map setToList sets
+   where setToList = case dir of ASC -> NSet.toAscList; DESC -> NSet.toDescList
+   
+   
+null :: LazySet a -> Bool
+null (LazySet dir (x:xs)) = True
+null _ = False
+
+size = sum . toList 
--- LazySetTest.hs
+++ LazySetTest.hs
@@ -2,31 +2,46 @@ import Test.HUnit
 import System.Exit
 import LazySet
 
-tests = TestList [basic, infinity]
+tests = TestList [basic, desc, infinity]
 
 
 ---- BASIC -----
 
 basic = TestList [noZero, oneToTen, noEleven]
+    where   
+    toTenList = [1..10]
+    toTenSet = fromList toTenList
 
-toTenList = [1..10]
-toTenSet = fromList toTenList
+    noZero   = TestCase $ assertBool "Zero not in there" $not (member 0 toTenSet)
+    oneToTen = TestCase $ assertBool "1 to 10 present in set." $ all (\i -> member i toTenSet) toTenList
+    noEleven = TestCase $ assertBool "11 not in there" $not (member 11 toTenSet)
 
-noZero   = TestCase $ assertBool "Zero not in there" $not (member 0 toTenSet)
-oneToTen = TestCase $ assertBool "1 to 10 present in set." $ all (\i -> member i toTenSet) toTenList
-noEleven = TestCase $ assertBool "11 not in there" $not (member 11 toTenSet)
+---DESC-------
+desc = TestList [noZero2, oneToMinusTen, noMinusEleven]
+    where 
+    toMinusTenList = map negate [1..10]
+    toMinusTenSet = fromListDesc toMinusTenList
 
+    noZero2   = TestCase $ assertBool "Zero not in there" $not (member 0 toMinusTenSet)
+    oneToMinusTen = TestCase $ assertBool "1 to 10 present in set." $ all (\i -> member i toMinusTenSet) toMinusTenList
+    noMinusEleven = TestCase $ assertBool "11 not in there" $not (member (-11) toMinusTenSet)
 
------ INFINITY
+----- INFINITY----
 
                      
-evenNumberSet = fromList $ filter even [1..]                     
+evenNumberSet = fromList $ filter even [1..]         
+
+negativeNumerSet = fromListDesc $ map negate [1..]            
                      
 infinity = TestList [
     TestCase ( assertBool "Even numbers are in there " 
         (all (\i -> member i evenNumberSet) [2,4,6,100,10^4])),
     TestCase ( assertBool "Odd numbers are in there " 
-        (all (\i ->not $ member i evenNumberSet) [1,3,5,99,10^4+1]))
+        (all (\i ->not $ member i evenNumberSet) [1,3,5,99,10^4+1])),
+    TestCase (assertBool "One not Present" 
+        $ not $ member 1 negativeNumerSet),
+    TestCase (assertBool "Minues 1000 present"
+        $ member (-1000) negativeNumerSet)
     ]
         
                      
