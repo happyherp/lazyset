622e5c65715d8dc4d49c23eb0aa9d419aea5c3dd
Carlos Freund <carlosfreund@gmail.com>
2016-11-25 17:55:53 +0100

    first 100k primes work

--- Prime.hs
+++ Prime.hs
@@ -40,7 +40,7 @@ products n factors = let
     in myMergeAll $  map f factors    
 -}  
 
-{-
+
 products :: Integral a => Show a => Int -> [a] -> [a]
 products n factors = map product $  products2 n factors
 
@@ -50,20 +50,25 @@ products2 1 factors = map (:[]) factors
 products2 n factors = let 
     step (lowest:rest) = map (lowest:) $ products2 (n-1) (lowest:rest)
     in myMergeAllBy (comparing product) $ map step $ init $ tails factors
+    
+    
+composites = myMergeAll $  map (\i->products i primes) [2..]
 
--}
-
-allProducts ::  Integral a => Show a => [a] -> [[a]]
+{-
+allProducts ::  Integral a => Show a => [a] -> [a]
 allProducts factors =  let 
-    productsByFactors = iterate addFactor $ map (:[]) factors
-    addFactor products = myMergeAllBy (comparing product) $ map (\f->[f:term | term<-products, f <= head term]) factors 
-    in myMergeAllBy (comparing product) (tail productsByFactors)
+    productsByFactors = iterate addFactors $ map (:[]) factors
+    addFactors products = let addFactor f = [f:term | term<-products, f <= head term]
+                          in  myMergeAllBy (comparing product) $ map addFactor factors 
+    in myMergeAll $ map (map product) (tail productsByFactors)
     
     
-composites = map product $ allProducts primes
+composites = allProducts primes
+                -}
+                
                 
 primes :: [Integer]
-primes = 2:(minus [3..] composites)
+primes = 2:([3..] `minus` composites)
 
 isPrime :: Integer -> Bool
 isPrime n = member n primes
