37e21e0763bbe1e3ba2d1235bc545c0162d4e284
Carlos Freund <carlosfreund@gmail.com>
2016-12-13 20:20:53 +0100

    test with fizzbuzz

--- LazySetTest.hs
+++ LazySetTest.hs
@@ -2,7 +2,8 @@ import Test.HUnit
 import System.Exit
 import LazySet
 
-tests = TestList [basic, {- desc, -}infinity]
+tests = TestList [basic, fizzbuzztest, infinity]
+
 
 
 ---- BASIC -----
@@ -15,36 +16,31 @@ basic = TestList [noZero, oneToTen, noEleven]
     noZero   = TestCase $ assertBool "Zero not in there" $not (member 0 toTenSet)
     oneToTen = TestCase $ assertBool "1 to 10 present in set." $ all (\i -> member i toTenSet) toTenList
     noEleven = TestCase $ assertBool "11 not in there" $not (member 11 toTenSet)
-
----DESC-------
-{-
-desc = TestList [noZero2, oneToMinusTen, noMinusEleven]
-    where 
-    toMinusTenList = map negate [1..10]
-    toMinusTenSet = fromDescList toMinusTenList
-
-    noZero2   = TestCase $ assertBool "Zero not in there" $not (member 0 toMinusTenSet)
-    oneToMinusTen = TestCase $ assertBool "1 to 10 present in set." $ all (\i -> member i toMinusTenSet) toMinusTenList
-    noMinusEleven = TestCase $ assertBool "11 not in there" $not (member (-11) toMinusTenSet)
--}
+    
+    
 ----- INFINITY----
-
-                     
+                    
 evenNumberSet = fromList $ filter even [1..]         
-
-{-negativeNumerSet = fromDescList $ map negate [1..] -}
                      
 infinity = TestList [
     TestCase ( assertBool "Even numbers are in there " 
         (all (\i -> member i evenNumberSet) [2,4,6,100,10^4])),
     TestCase ( assertBool "Odd numbers are in there " 
-        (all (\i ->not $ member i evenNumberSet) [1,3,5,99,10^4+1])){-,
+        (all (\i ->not $ member i evenNumberSet) [1,3,5,99,10^4+1]))
+    ] 
+    
+        
+---  FizzBuzz ---
 
-        TestCase (assertBool "One not Present" 
-        $ not $ member 1 negativeNumerSet),
-    TestCase (assertBool "Minues 1000 present"
-        $ member (-1000) negativeNumerSet)  -}
-    ]
+isFizzBuzz x = x `mod` 7 == 0 || '7' `elem` show x       
+
+fizzbuzzes = fromList $ filter isFizzBuzz [1..]
+
+fizzbuzztest = TestList[
+    TestCase ( assertBool "fizzes" 
+        (all (\i ->       member i fizzbuzzes) [7,14,21,70,71,77])),
+    TestCase ( assertBool "does not fizz" 
+        (all (\i -> not $ member i fizzbuzzes) [1,2,3,8,9,10,16,22,100]))    ]
         
                      
 main = do
