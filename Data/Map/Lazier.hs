2ae7e6056f4d586073430f5449f5175b30027a2e
Carlos Freund <carlosfreund@gmail.com>
2016-12-15 12:37:38 +0100

    Haddock documentation

--- Data/Map/Lazier.hs
+++ Data/Map/Lazier.hs
@@ -1,8 +1,31 @@
-module Data.Map.Lazier where 
+{-| 
+Copyright   : (c) Carlos Freund, 2016
+License     : MIT
+Maintainer  : carlosfreund@gmail.com
+Stability   : experimental
+
+A Map that can be created from lazy, ordered, infinite lists. 
+Based on 'Data.Set.Lazy.LazySet'.
+-}
+module Data.Map.Lazier (
+
+    -- * Types 
+    Map,
+    
+    -- * Query 
+    member,
+    lookup,
+    
+    -- * Creation
+    fromList
+    
+    )where 
 
 import qualified Data.Set.Lazy as LS
 import Data.Ord(compare)
+import Prelude hiding(lookup)
 
+-- | A mapping of type 'k' to type 'e'.
 type Map k e = LS.LazySet (MapEntry k e) 
 
 data MapEntry k e = MapEntry k e | JustKey k
@@ -21,13 +44,15 @@ getKey (JustKey k) = k
 entryFromTuple :: Ord k => (k,e) -> MapEntry k e
 entryFromTuple (k,e) = MapEntry k e
     
+-- | Create a new Map from a list of tuples. The list must be sorted by key ascending.     
 fromList :: Ord k => [(k,e)] -> Map k e
 fromList xs = LS.fromList $ map entryFromTuple xs
 
-
+-- | Check if a key exists in this Map. 
 member :: Ord k => k -> Map k e -> Bool
 member key = LS.member (JustKey key)
 
+-- | Return the value of a given key or 'Nothing'
 lookup :: Ord k => k -> Map k e -> Maybe e
 lookup k map=  case LS.lookup (JustKey k) map of 
     Just (MapEntry _ e) -> Just e 
--- Data/Set/Lazy.hs
+++ Data/Set/Lazy.hs
@@ -1,4 +1,13 @@
+{-| 
+Module      : LazySet
+Description : A truly lazy Set. 
+Copyright   : (c) Carlos Freund, 2016
+License     : MIT
+Maintainer  : carlosfreund@gmail.com
+Stability   : experimental
 
+A Set that can be created from lazy, ordered, infinite lists. 
+-}
 module Data.Set.Lazy where 
 
 import Prelude hiding(lookup)
@@ -12,66 +21,100 @@ import Data.Ord
 data LazySet a = LazySet [NSet.Set a]
     deriving (Eq, Show)
     
-empty :: Ord a => LazySet a    
-empty = fromList []
+-- * Query    
+
+-- | Checks if the value is a member of the 'LazySet'. 
+-- Performance: O(m)=log m  
+--Where m is the position of the element beeing searched for. 
+-- This only applies after the element has been fetched from the underlying list.
+member :: Ord a => a -> LazySet a ->  Bool
+member e set = isJust $ lookup e set             
     
+-- | Searches for a value in a Set. If it can not be found returns 'Nothing' otherwhise 
+-- Returns 'Just a' if it can find it. The returned value will be the one from the set, not the one that was passed. 
 lookup :: Ord a => a -> LazySet a -> Maybe a
 lookup e (LazySet sets) = findIn sets
     where findIn [] = Nothing
           findIn (set:rest) = case NSet.lookupLE e set of 
             Nothing -> Nothing
             Just x ->  if (x == e) then Just x else findIn rest  
+
+
+-- | Returns true if the 'LazSet' is empty. 
+null :: LazySet a -> Bool
+null (LazySet (x:xs)) = True
+null _ = False
+
+-- | Returns the size of the set. Do not use this on infinite Sets.
+size :: Ord a => LazySet a -> Int
+size = length . toList 
+
+-- * Combine
     
-member :: Ord a => a -> LazySet a ->  Bool
-member e set = isJust $ lookup e set
-      
-
-
+-- | Splits the 'LazySet' into two parts. 
+-- The first containing all consecutive elements of the Set where the predicate applies.
+-- The second contains the (infinite) rest. 
 spanAntitone :: Ord a => (a -> Bool) -> LazySet a -> (LazySet a, LazySet a)
 spanAntitone pred (LazySet sets) = let
     (lesser, (middle:higher)) = List.span (pred . NSet.findMax) sets
     (middleLesser, middleHigher) = NSet.spanAntitone pred middle
     in (LazySet (lesser++[middleLesser]), LazySet (middleHigher:higher))
     
-    
+-- | Union of two LazySets.     
 union :: Ord a => LazySet a -> LazySet a -> LazySet a
 union s1 s2 = let  
     in fromList $ OList.union (toList s1) (toList s2)
 
 
+-- * Build
 
-build :: Ord a => Int -> Float -> [a] -> [NSet.Set a]         
+
+-- | Create an 'empty' 'LazySet'.    
+empty :: Ord a => LazySet a    
+empty = fromList []
+    
+  
+-- | Builds a 'LazySet' from an ascending ordered list.
+-- If the list is not ordered an error is thrown. 
+fromAscList :: Ord a => [a] -> LazySet a
+fromAscList = growFromAscList 2.0               
+
+
+-- | Like 'fromAscList' but with a custom growth-factor. 
+growFromAscList :: Ord a => 
+    Float   -- ^ The factor by which the subtrees grow. 
+    --Must be >= 1.0. A growth of 1.0 makes the 'LazySet' behave like a List. 
+    -- The higher it is set, the more it behaves like a 'Data.Set'.
+    -- The downside of a higher growth-factor is that bigger batches are extracted from the source-list at once. 
+    -> [a]  -- ^ An ascending List
+    -> LazySet a     
+growFromAscList growth _ | growth < 1.0 = error "growth must be at least 1" 
+growFromAscList growth xs = LazySet (build 0 growth (checkDir xs))
+    where checkDir (a:b:s)| a > b = error "Elements must be ascending." 
+          checkDir  xs = xs   
+
+-- | Alias for 'fromAscList'.
+fromList :: Ord a => [a] -> LazySet a
+fromList = fromAscList
+          
+-- | Create a 'LazSet' from a descending list.           
+fromDescList :: Ord a => [a] -> LazySet (Down a)
+fromDescList xs = fromAscList (map Down xs)
+    
+-- | Kind of internal. 
+build :: Ord a => Int -- ^ starting-depth 
+    -> Float -- ^ growth-factor
+    -> [a] -- ^Ascending source-list 
+    -> [NSet.Set a]         
 build _ _ [] = []
 build level growth xs = let 
     (elementsForThisLevel, elementsFurtherDown) = List.splitAt (ceiling $ growth^level) xs
     in (NSet.fromAscList elementsForThisLevel):(build (level + 1) growth elementsFurtherDown)
     
     
-fromAscList :: Ord a => [a] -> LazySet a
-fromAscList = growFromAscList 2.0               
-
-growFromAscList :: Ord a => Float -> [a] -> LazySet a     
-growFromAscList growth _ | growth < 1.0 = error "growth must be at least 1" 
-growFromAscList growth xs = LazySet (build 0 growth (checkDir xs))
-    where checkDir (a:b:s)| a > b = error "Elements must be ascending." 
-          checkDir  xs = xs   
-
-fromList :: Ord a => [a] -> LazySet a
-fromList = fromAscList
-          
-          
-fromDescList :: Ord a => [a] -> LazySet (Down a)
-fromDescList xs = fromAscList (map Down xs)
-          
+-- * Export
       
--- List with all elements in the order of the set.
+-- | List with all elements in order.
 toList :: Ord a => LazySet a -> [a]
 toList (LazySet sets) = concat $ map NSet.toAscList sets
    
-   
-null :: LazySet a -> Bool
-null (LazySet (x:xs)) = True
-null _ = False
-
-size = sum . toList 
-
